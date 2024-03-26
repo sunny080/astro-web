a7c935e1a649eda22ad157dfb86f96cf09de4d98
aqilusman45 <aqilusman45@gmail.com>
2024-03-26 11:34:02 +0500

    fix:

---
+++ studio/schemas/index.js
@@ -0,0 +1,6 @@
+import blockContent from './documents/blockContent'
+import category from './documents/category'
+import post from './documents/post'
+import author from './documents/author'
+
+export const schemaTypes = [post, author, category, blockContent]
